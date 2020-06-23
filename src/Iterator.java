226e9fe9b6ec75f11a6c530885e96e094dd2d8a7
Zarubin Ilya <ilyazaru@mail.ru>
2020-06-23 15:09:26 +0300

    Commit #3 - Changed class Iterator

--- src/Iterator.java
+++ src/Iterator.java
@@ -1,8 +1,13 @@
 public class Iterator {
 
     public void printIterator(int count){
+        String episode = "Episode";
         for (int i=0;i<count+1;i++){
-            System.out.println("Episode "+i);
+            if (i<=episode.length()-1){
+                System.out.println("Episode"+i+" "+episode.charAt(i));
+            }
+            else System.out.println("Episode"+i+" none");
+
         }
     }
 }
