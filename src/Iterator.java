dcc5e22936427b1bda5522f75059b21d79bc7ccf
Zarubin Ilya <ilyazaru@mail.ru>
2020-06-23 14:37:50 +0300

    Commit #2 - Added Iterator

---
+++ src/Iterator.java
@@ -0,0 +1,8 @@
+public class Iterator {
+
+    public void printIterator(int count){
+        for (int i=0;i<count+1;i++){
+            System.out.println("Episode "+i);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Iterator iter = new Iterator();
+        iter.printIterator(5);
     }
 }
